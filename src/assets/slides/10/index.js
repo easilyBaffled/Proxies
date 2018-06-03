d12f5dd9cc617eec96da63d4cb5587b97bd5976f
Danny Michaelis <dmichaelis0@gmail.com>
2018-06-03 14:07:43 -0400

    + clean up console

--- src/assets/slides/10/index.js
+++ src/assets/slides/10/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { CodePane, Code, Slide } from 'spectacle';
+import { CodePane, Slide } from 'spectacle';
 import { stripIndent } from 'common-tags';
 import { pipe } from 'ramda';
 import { arrayOf, string, number } from 'prop-types';
 
 import { addLineNumbers } from '../../../util';
-import { isA } from '../../../proxies';
+import Console from '../../../components/util/Console';
 
 import trick from './trick';
 
 const increment = maxValue => ( { i } ) => ( { i: Math.min( i + 1, maxValue ) } );
 const decrement = ( { i } ) => ( { i: Math.max( i - 1, 0 ) } );
-
-const push = propName => ( ...newItems ) => state => (
-    { [ propName ]: [ ...state[ propName ], ...newItems ] }
-);
-const pushLogs = push( 'logs' );
 
 const formatCodeString = pipe(
     func => func.toString().replace( /^function\s*\(\)\s*{\n?/, '' ).replace( /}$/, '' ),
@@ -42,18 +37,11 @@ class Ex extends React.Component
     };
 
     state =  {
-        i: 0,
-        logs: []
+        i: 0
     };
 
     boundInc = increment( this.props.slideLines.length - 1 );
     codeLines = this.props.code.split( '\n' );
-
-    log = ( ...args ) =>
-    {
-        const stringifiedArgs = args.map( arg => isA.string( arg ) ? arg : JSON.stringify( arg, null, 4 ) ).join( ', ' );
-        this.setState( pushLogs( stringifiedArgs ) );
-    };
 
     render ()
     {
@@ -64,28 +52,30 @@ class Ex extends React.Component
         const lengthBasedTextSize = getFontSize( 16 )( displayStr.length - 1 );
         console.log( { lengthBasedTextSize } );
         return (
-            <React.Fragment>
-                <div className='nice-code'>
-                    <CodePane overflow='hidden'
-                        height={ ( lengthBasedTextSize + 9 ) * displayStr.length }
-                        textSize={ lengthBasedTextSize }
-                        key='nice-code'
-                        theme='light'
-                        lang='javascript'
-                        source={ this.props.code /* displayStr.join( '\n' ) */ }
-                    />
-                    <div className='left' onClick={ () => this.setState( decrement ) } />
-                    <div className='right' onClick={ () => this.setState( this.boundInc ) } />
-                </div>
-                <button onClick={ () => this.log( displayStr.join( '\n' ) ) } >Save</button>
-                <CodePane theme='dark'
-                    lang='javascript'
-                    source={ this.state.logs.join( '\n\n' ) }
-                />
-            </React.Fragment>
+
+            <Console render={
+                ( { log } ) => (
+                    <React.Fragment>
+                        <div className='nice-code'>
+                            <CodePane overflow='hidden'
+                                height={ ( lengthBasedTextSize + 9 ) * displayStr.length }
+                                textSize={ lengthBasedTextSize }
+                                key='nice-code'
+                                theme='light'
+                                lang='javascript'
+                                source={ this.props.code /* displayStr.join( '\n' ) */ }
+                            />
+                            <div className='left' onClick={ () => this.setState( decrement ) } />
+                            <div className='right' onClick={ () => this.setState( this.boundInc ) } />
+                        </div>
+                        <button onClick={ () => log( displayStr.join( '\n' ) ) } >Save</button>
+                    </React.Fragment>
+                )
+
+            } />
         );
     }
-};
+}
 
 export default (
     <Slide background='linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%)'>
--- src/components/util/Console.js
+++ src/components/util/Console.js
@@ -1,44 +1,22 @@
 import React from 'react';
-import { CodePane, Code, Slide } from 'spectacle';
+import { CodePane } from 'spectacle';
 import { stripIndent } from 'common-tags';
-import { pipe } from 'ramda';
-import { arrayOf, string, number } from 'prop-types';
 import { renderProps } from 'react-powerplug';
 
-import { addLineNumbers } from '../../../util';
-import { isA } from '../../../proxies';
+import { isA } from '../../proxies';
 
-import trick from './trick';
-
-const increment = maxValue => ( { i } ) => ( { i: Math.min( i + 1, maxValue ) } );
-const decrement = ( { i } ) => ( { i: Math.max( i - 1, 0 ) } );
-
-const push = propName => ( ...newItems ) => state => (
-    { [ propName ]: [ ...state[ propName ], ...newItems ] }
-);
-const pushLogs = push( 'logs' );
-
-const formatCodeString = pipe(
-    func => func.toString().replace( /^function\s*\(\)\s*{\n?/, '' ).replace( /}$/, '' ),
-    stripIndent,
-    addLineNumbers
+const update = propName => updater => state => (
+    { [ propName ]: updater( state[ propName ], state ) }
 );
 
-const exampleCode = formatCodeString( trick );
+const push = new Proxy( {}, {
+    get: ( _, propName ) =>
+        ( ...newItems ) =>
+            update( propName )( arr => [ ...arr, ...newItems ] )
+} );
 
-const getFontSize = baseFontSize => lineNum =>
+export default class Console extends React.Component
 {
-    console.log( baseFontSize - lineNum + baseFontSize );
-    return Math.max(
-        Math.min( baseFontSize - lineNum + baseFontSize, 32 ),
-        baseFontSize
-    );
-};
-
-class Ex extends React.Component
-{
-    static propTypes = {};
-
     state =  {
         logs: []
     };
@@ -46,7 +24,7 @@ class Ex extends React.Component
     log = ( ...args ) =>
     {
         const stringifiedArgs = args.map( arg => isA.string( arg ) ? arg : JSON.stringify( arg, null, 4 ) ).join( ', ' );
-        this.setState( pushLogs( stringifiedArgs ) );
+        this.setState( push.logs( stringifiedArgs ) );
     };
 
     clear = () =>
@@ -66,15 +44,9 @@ class Ex extends React.Component
                 }
                 <CodePane theme='dark'
                     lang='javascript'
-                    source={ this.state.logs.join( '\n\n' ) }
+                    source={ stripIndent( this.state.logs.join( '\n\n' ) ) }
                 />
             </React.Fragment>
         );
     }
 };
-
-export default (
-    <Slide background='linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%)'>
-        <Ex code={ exampleCode } slideLines={ [ [], [ 1, 10 ], [ 13, 16 ], [ 18, 18 ], [ 20, 21 ] ] } />
-    </Slide>
-);
